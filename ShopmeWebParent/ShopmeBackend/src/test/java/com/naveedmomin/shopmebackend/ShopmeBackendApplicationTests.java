f1ea0e891ece73efaca3cbcbef32e68946f6d77c
Naveed Momin <naddy19@gmail.com>
2021-11-09 00:37:00 +0400

    Initial Commit

---
+++ ShopmeWebParent/ShopmeBackend/src/test/java/com/naveedmomin/shopmebackend/ShopmeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naveedmomin.shopmebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
