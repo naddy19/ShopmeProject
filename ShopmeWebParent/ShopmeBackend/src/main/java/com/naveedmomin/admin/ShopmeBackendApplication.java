1abcf29b4c6127f6f3600213d56276626f84517b
Naveed Momin <naddy19@gmail.com>
2021-11-09 02:06:55 +0400

    Adding shopme frontend project

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/naveedmomin/admin/ShopmeBackendApplication.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/naveedmomin/admin/ShopmeBackendApplication.java
@@ -1,4 +1,4 @@
-package com.naveedmomin.shopmebackend;
+package com.naveedmomin.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
